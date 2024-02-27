11efbfa1c902808c5c329b55e9c34f204013ed72
Konrad Moron <konrad.moron@tum.de>
2024-02-27 20:55:44 +0100

    Remove hardcoded home paths

--- res/rv32gc-toolchain.cmake
+++ res/rv32gc-toolchain.cmake
@@ -5,7 +5,7 @@ set(RISCV_ARCH "rv32gc" CACHE STRING "RISC-V architecture (-march)")
 set(RISCV_ABI "ilp32d" CACHE STRING "RISC-V ABI (-mabi)")
 
 set(RISCV_TOOLCHAIN_PREFIX "" CACHE STRING "optional prefix for the riscv toolchain in case it is not on the path")
-set(RISCV_TOOLCHAIN_BASENAME "riscv32-unknown-elf" CACHE STRING "base name of the toolchain executables")
+set(RISCV_TOOLCHAIN_BASENAME "riscv32" CACHE STRING "base name of the toolchain executables")
 
 
 if("${RISCV_TOOLCHAIN_PREFIX}" STREQUAL "")
@@ -19,10 +19,10 @@ endif()
 set(CMAKE_C_COMPILER "${RISCV_TOOLCHAIN}-clang${EXE_EXT}")
 set(CMAKE_CXX_COMPILER "${RISCV_TOOLCHAIN}-clang++${EXE_EXT}")
 
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -D__riscv__ -march=${RISCV_ARCH} -mabi=${RISCV_ABI} -fno-builtin-bcmp -Oz -I/home/ahc/Desktop/CodeComp/integrator/embench-iot/support -I/home/ahc/Desktop/CodeComp/integrator/embench-iot/config/riscv32/boards/ri5cyverilator -I/home/ahc/Desktop/CodeComp/integrator/embench-iot/config/riscv32/chips/generic -I/home/ahc/Desktop/CodeComp/integrator/embench-iot/config/riscv32 -DCPU_MHZ=1 -DWARMUP_HEAT=1")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -D__riscv__ -march=${RISCV_ARCH} -mabi=${RISCV_ABI} -fno-builtin-bcmp -Oz -I/home/ahc/Desktop/CodeComp/integrator/embench-iot/support -I/home/ahc/Desktop/CodeComp/integrator/embench-iot/config/riscv32/boards/ri5cyverilator -I/home/ahc/Desktop/CodeComp/integrator/embench-iot/config/riscv32/chips/generic -I/home/ahc/Desktop/CodeComp/integrator/embench-iot/config/riscv32 -DCPU_MHZ=1 -DWARMUP_HEAT=1")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -D__riscv__ -march=${RISCV_ARCH} -mabi=${RISCV_ABI} -fno-builtin-bcmp -Oz")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -D__riscv__ -march=${RISCV_ARCH} -mabi=${RISCV_ABI} -fno-builtin-bcmp -Oz")
 set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} -D__riscv__ -march=${RISCV_ARCH} -mabi=${RISCV_ABI}")
-set(CMAKE_EXE_LINKER_FLAGS "-march=${RISCV_ARCH} -mabi=${RISCV_ABI} -nostartfiles -nostdlib /home/ahc/Desktop/CodeComp/integrator/embench-iot/bd/config/riscv32/chips/generic/chipsupport_etiss.o /home/ahc/Desktop/CodeComp/integrator/embench-iot/bd/config/riscv32/boards/ri5cyverilator/boardsupport_etiss.o /home/ahc/Desktop/CodeComp/integrator/embench-iot/bd/support/main_etiss.o /home/ahc/Desktop/CodeComp/integrator/embench-iot/bd/support/beebsc_etiss.o /home/ahc/Desktop/CodeComp/integrator/embench-iot/bd/support/dummy-crt0_etiss.o    /home/ahc/Desktop/CodeComp/integrator/embench-iot/bd/support/dummy-libc_etiss.o /home/ahc/Desktop/CodeComp/integrator/embench-iot/bd/support/dummy-libgcc_etiss.o /home/ahc/Desktop/CodeComp/integrator/embench-iot/bd/support/dummy-libm_etiss.o")
+set(CMAKE_EXE_LINKER_FLAGS "-march=${RISCV_ARCH} -mabi=${RISCV_ABI} -nostartfiles -nostdlib")
 
 ## Build
 #/opt/riscv-elf/bin/clang 
